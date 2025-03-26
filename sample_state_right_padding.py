3342d79f4669e4dcc7434f521a1afcf580a56269
john <1165137798@qq.com>
2025-03-26 18:34:53 +0800

    commit

--- sample_state_right_padding.py
+++ sample_state_right_padding.py
@@ -289,10 +289,9 @@ class Sampler:
 
 
 
-        next_token_logits=jnp.take_along_axis(logits,position_ids[...,None]-1,axis=1)[-1]
+        next_token_logits=jnp.take_along_axis(logits,position_ids[...,None]-1,axis=1)[:,-1]
         # next_token_predict = jnp.argmax(, axis=-1)[:,0]
-        print(next_token_logits.shape)
-        next_token_predict=self.sample_fn(self.key,next_token_logits)[:,0]
+        next_token_predict=self.sample_fn(self.key,next_token_logits)
         print(next_token_predict.shape)
 
 
--- test_place.py
+++ test_place.py
@@ -12,8 +12,12 @@ def _top_k_sampling_batched(rng, logits, k=50, t=0.9):
     top_logits, top_indices = jax.lax.top_k(logits_single, k)
     # 在 top_logits 上进行 categorical 采样，返回采样到的相对索引
     sampled_relative_idx = jax.random.categorical(rng, top_logits)
+
+
+    print(logits_single.shape)
+
     # 根据采样结果，从 top_indices 中取出对应的绝对索引
-    return top_indices[sampled_relative_idx]  # 修复索引方式
+    return top_indices[jnp.arange(0,logits_single.shape[0]),sampled_relative_idx]  # 修复索引方式
 
   # 分割 rng 以获得每个 batch 的独立随机数种子
   batch_size = logits.shape[0]
@@ -24,6 +28,6 @@ def _top_k_sampling_batched(rng, logits, k=50, t=0.9):
 
 
 key=jax.random.PRNGKey(3)
-x=jax.random.uniform(key,(2,128))
+x=jax.random.uniform(key,(2,1,128))
 
-print(_top_k_sampling_batched(key,x))
+print(_top_k_sampling_batched(key,x).shape)

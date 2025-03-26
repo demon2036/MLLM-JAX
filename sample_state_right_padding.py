63595f62f5938dde25bd8fa791b0864874f9581e
john <1165137798@qq.com>
2025-03-26 18:52:24 +0800

    commit

--- sample_state_right_padding.py
+++ sample_state_right_padding.py
@@ -223,7 +223,13 @@ class Sampler:
 
 
 
-        global_length=jnp.max(process_allgather(input_ids.shape[1]))
+        # global_length=jnp.max(process_allgather(input_ids.shape[1]))
+
+        global_length=512
+
+
+        # _form_global_array(np.array(input_ids.shape[1]))
+
 
         prefill_length = self.find_ceil(global_length)
         # prefill_length = input_ids.shape[1]

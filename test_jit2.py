d98d134bf765ab1bc41ccfa61b2c24843ce4a932
john <1165137798@qq.com>
2025-03-27 10:34:00 +0800

    commit

--- test_jit2.py
+++ test_jit2.py
@@ -52,121 +52,10 @@ def slice_data(x,accumulate_steps,i):
     assert b%accumulate_steps==0
     micro_batch_size=b//accumulate_steps
     data=x[i*micro_batch_size:(i+1)*micro_batch_size]
-    # print(data.shape)
+    print(data.shape)
     return data
 
 
-class TrainState(train_state.TrainState):
-    micro_step: int = 0
-    micro_in_mini: int = 1
-    grad_accum: ArrayTree | None = None
-    ref_params:Any=None
-
-
-def get_state(mesh,training_steps=100):
-    model, params, tokenizer = get_model(mesh,model_path=model_path, )
-    model_ref = get_model(mesh, model_path=model_path, only_model=True)
-
-    beta=0.0
-
-    train_module = TrainGRPOModule(model=model, pad_token_id=tokenizer.pad_token_id,ref_model=model_ref,num_pre_Q=num_pre_Q,beta=beta)
-    def init_fn(params):
-
-        learning_rate = optax.warmup_cosine_decay_schedule(
-            init_value=1e-7,
-            peak_value=1e-6,
-            warmup_steps=int(training_steps*0.05),
-            decay_steps=training_steps,
-            end_value=1e-7,
-        )
-        tx = optax.adamw(learning_rate)
-        # tx = optax.lion(learning_rate)
-        # tx = optax.sgd(learning_rate)
-        if grad_accum_steps > 1:
-            print(f'{grad_accum_steps=}')
-            grad_accum = jax.tree_map(jnp.zeros_like, params)
-
-
-        return TrainState.create(apply_fn=train_module.apply,params=params,tx=tx,
-                                 ref_params=copy.deepcopy(params) if beta==0 else None,
-                                 micro_step=0,
-                                 micro_in_mini=grad_accum_steps,
-                                 grad_accum=grad_accum if grad_accum_steps > 1 else None,
-                                 )
-
-
-    state_shapes = jax.eval_shape(init_fn, params, )
-    train_state_partition = match_partition_rules(get_partition_rules_llama(), state_shapes)
-    train_state_sharding = jax.tree_util.tree_map(lambda x: jax.sharding.NamedSharding(mesh, x), train_state_partition)
-    state = jax.jit(init_fn, donate_argnums=(0,),out_shardings=train_state_sharding)(params)
-
-    sampler = Sampler(model, tokenizer,mesh=mesh,)
-
-
-    return state,sampler,train_state_sharding
-
-
-
-
-def training_step(state: TrainState, inputs: ArrayTree,) -> tuple[TrainState, ArrayTree]:
-
-    def loss_fn(params: ArrayTree) -> ArrayTree:
-        metrics=state.apply_fn({'params': {'model':params,'ref_model':state.ref_params }, },inputs)
-        metrics = jax.tree_map(jnp.mean, metrics)
-        return metrics["loss"], metrics
-
-    def update_fn(state: TrainState) -> TrainState:
-        # Collect a global gradient from the accumulated gradients and apply actual
-        # parameter update with resetting the accumulations to zero.
-        grads = jax.tree_map(lambda g: g / state.micro_in_mini, state.grad_accum)
-        state = state.apply_gradients(
-            grads=grads,
-            grad_accum=jax.tree_map(jnp.zeros_like, state.grad_accum),
-            micro_step=state.micro_step % state.micro_in_mini,
-        )
-        return state
-
-
-    (_, metrics), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
-
-    # Update parameters with the gradients. If the gradient accumulation is enabled,
-    # then the parameters will be updated at the end of each mini-batch step. In every
-    # micro steps, the gradients will be accumulated.
-    if state.grad_accum is None:
-        state = state.apply_gradients(grads=grads)
-    else:
-        state = state.replace(
-            grad_accum=jax.tree_map(lambda ga, g: ga + g, state.grad_accum, grads),
-            micro_step=state.micro_step + 1,
-        )
-        state = jax.lax.cond(
-            state.micro_step == state.micro_in_mini, update_fn, lambda x: x, state
-        )
-    return state, metrics
-
-
-
-
-def repeat(lst:list,repeats:int):
-    return [x    for x in lst  for _ in range(repeats) ]
-
-
-
-def reward_correct(item, answer):
-    pattern = r'\d+\.\d+|\d+/\d+|\d+'
-    nums = re.findall(pattern, answer) # 使用正则表达式在answer中查找所有数字
-    if len(nums) == 0: return -1.0
-    lastnum = nums[-1] # 用answer中最后一个数字和ground_truth做比较
-    ans = parse(lastnum, extraction_config=[ExprExtractionConfig()])
-    ground_truth = parse(item["A"], extraction_config=[ExprExtractionConfig()])
-    # print(item["A"],ans)
-
-    return 1 if verify(ans, ground_truth) else -1
-def reward_format(item, answer):
-    # pattern = r"^<think>(?:(?!</?think>)[\s\S]*?)</think>\s*<answer>(?:(?!</?answer>)[\s\S]*?)</answer><\|im_end\|>$"
-    pattern = r"^<think>.*?</think>.*?<answer>.*?</answer>$"
-    # return 0.75 if re.match(pattern, answer, re.DOTALL | re.VERBOSE) else -0.5
-    return 1.25 if re.match(pattern, answer, re.DOTALL | re.VERBOSE) else -1
 
 
 
@@ -207,9 +96,7 @@ def gen_answers_jax(prompts,sampler,params):
                                         skip_special_tokens=True,
                                         )
 
-    # for ans in answers:
-    #     print(ans,len(ans))
-    #     print('\n'*2,flush=True)
+
 
     if jax.process_index()==0:
         print(answers[-2:])
---
+++ training.py
@@ -0,0 +1,173 @@
+import copy
+import os
+from typing import Any
+
+import tqdm
+from jax.experimental.multihost_utils import process_allgather
+
+os.environ['JAX_TRACEBACK_FILTERING']='off'
+
+
+import random
+import re
+from functools import partial
+
+import jax.tree_util
+import numpy as np
+import optax
+import torch
+from chex import ArrayTree
+from datasets import load_dataset
+from flax.training import train_state
+from jax import NamedSharding
+from jax._src.mesh import Mesh
+from jax._src.partition_spec import PartitionSpec
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+
+from MLLM_JAX.language.llama.llama import convert_torch_to_flax_llama
+from MLLM_JAX.language.qwen2.modular_qwen2 import Qwen2ForCausalLM
+from MLLM_JAX.train_modules import TrainSFTModule, TrainGRPOModule
+from MLLM_JAX.utils import get_jax_mesh2, match_partition_rules, get_partition_rules_llama, _form_global_array
+from sample_state_right_padding import get_model, Sampler
+# from sample_state_left_padding import get_model, Sampler
+import jax.numpy as jnp
+from math_verify import parse, verify, ExprExtractionConfig
+
+
+max_prompt_length=400
+num_pre_Q=16
+MAX_LENGTH_SAMPLE=1024
+MAX_LENGTH=MAX_LENGTH_SAMPLE+512 #-128
+BATCH=8
+grad_accum_steps = 8
+
+model_path = 'Qwen/Qwen2.5-3B'
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+system_prompt = """You are a helpful assistant. A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the user with the answer.\
+The reasoning process and answer are enclosed within <think> </think> and<answer> </answer> tags, respectively, i.e., <think> reasoning process here </think><answer> answer here </answer>."""
+
+
+def slice_data(x,accumulate_steps,i):
+    b,*_=x.shape
+    assert b%accumulate_steps==0
+    micro_batch_size=b//accumulate_steps
+    data=x[i*micro_batch_size:(i+1)*micro_batch_size]
+    # print(data.shape)
+    return data
+
+
+
+
+
+
+class TrainState(train_state.TrainState):
+    micro_step: int = 0
+    micro_in_mini: int = 1
+    grad_accum: ArrayTree | None = None
+    ref_params:Any=None
+
+
+def get_state(mesh,training_steps=100):
+    model, params, tokenizer = get_model(mesh,model_path=model_path, )
+    model_ref = get_model(mesh, model_path=model_path, only_model=True)
+
+    beta=0.0
+
+    train_module = TrainGRPOModule(model=model, pad_token_id=tokenizer.pad_token_id,ref_model=model_ref,num_pre_Q=num_pre_Q,beta=beta)
+    def init_fn(params):
+
+        learning_rate = optax.warmup_cosine_decay_schedule(
+            init_value=1e-7,
+            peak_value=1e-6,
+            warmup_steps=int(training_steps*0.05),
+            decay_steps=training_steps,
+            end_value=1e-7,
+        )
+        tx = optax.adamw(learning_rate)
+        # tx = optax.lion(learning_rate)
+        # tx = optax.sgd(learning_rate)
+        if grad_accum_steps > 1:
+            print(f'{grad_accum_steps=}')
+            grad_accum = jax.tree_map(jnp.zeros_like, params)
+
+
+        return TrainState.create(apply_fn=train_module.apply,params=params,tx=tx,
+                                 ref_params=copy.deepcopy(params) if beta==0 else None,
+                                 micro_step=0,
+                                 micro_in_mini=grad_accum_steps,
+                                 grad_accum=grad_accum if grad_accum_steps > 1 else None,
+                                 )
+
+
+    state_shapes = jax.eval_shape(init_fn, params, )
+    train_state_partition = match_partition_rules(get_partition_rules_llama(), state_shapes)
+    train_state_sharding = jax.tree_util.tree_map(lambda x: jax.sharding.NamedSharding(mesh, x), train_state_partition)
+    state = jax.jit(init_fn, donate_argnums=(0,),out_shardings=train_state_sharding)(params)
+
+    sampler = Sampler(model, tokenizer,mesh=mesh,)
+
+
+    return state,sampler,train_state_sharding
+
+
+
+
+def training_step(state: TrainState, inputs: ArrayTree,) -> tuple[TrainState, ArrayTree]:
+
+    def loss_fn(params: ArrayTree) -> ArrayTree:
+        metrics=state.apply_fn({'params': {'model':params,'ref_model':state.ref_params }, },inputs)
+        metrics = jax.tree_map(jnp.mean, metrics)
+        return metrics["loss"], metrics
+
+    def update_fn(state: TrainState) -> TrainState:
+        # Collect a global gradient from the accumulated gradients and apply actual
+        # parameter update with resetting the accumulations to zero.
+        grads = jax.tree_map(lambda g: g / state.micro_in_mini, state.grad_accum)
+        state = state.apply_gradients(
+            grads=grads,
+            grad_accum=jax.tree_map(jnp.zeros_like, state.grad_accum),
+            micro_step=state.micro_step % state.micro_in_mini,
+        )
+        return state
+
+
+    (_, metrics), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+
+    # Update parameters with the gradients. If the gradient accumulation is enabled,
+    # then the parameters will be updated at the end of each mini-batch step. In every
+    # micro steps, the gradients will be accumulated.
+    if state.grad_accum is None:
+        state = state.apply_gradients(grads=grads)
+    else:
+        state = state.replace(
+            grad_accum=jax.tree_map(lambda ga, g: ga + g, state.grad_accum, grads),
+            micro_step=state.micro_step + 1,
+        )
+        state = jax.lax.cond(
+            state.micro_step == state.micro_in_mini, update_fn, lambda x: x, state
+        )
+    return state, metrics
+
+
+
+
+def repeat(lst:list,repeats:int):
+    return [x    for x in lst  for _ in range(repeats) ]
+
+
+
+def reward_correct(item, answer):
+    pattern = r'\d+\.\d+|\d+/\d+|\d+'
+    nums = re.findall(pattern, answer) # 使用正则表达式在answer中查找所有数字
+    if len(nums) == 0: return -1.0
+    lastnum = nums[-1] # 用answer中最后一个数字和ground_truth做比较
+    ans = parse(lastnum, extraction_config=[ExprExtractionConfig()])
+    ground_truth = parse(item["A"], extraction_config=[ExprExtractionConfig()])
+    # print(item["A"],ans)
+
+    return 1 if verify(ans, ground_truth) else -1
+def reward_format(item, answer):
+    # pattern = r"^<think>(?:(?!</?think>)[\s\S]*?)</think>\s*<answer>(?:(?!</?answer>)[\s\S]*?)</answer><\|im_end\|>$"
+    pattern = r"^<think>.*?</think>.*?<answer>.*?</answer>$"
+    # return 0.75 if re.match(pattern, answer, re.DOTALL | re.VERBOSE) else -0.5
+    return 1.25 if re.match(pattern, answer, re.DOTALL | re.VERBOSE) else -1

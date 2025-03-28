2f6f024c358a59c0f886a60c9c8015ff715da5da
john <1165137798@qq.com>
2025-03-28 09:52:27 +0800

    commit

--- sample_state_right_padding.py
+++ sample_state_right_padding.py
@@ -211,7 +211,7 @@ class Sampler:
         next_token_predict=jnp.where(sample_state.dones,self.tokenizer.eos_token_id,next_token_predict)
 
 
-        dones = sample_state.dones | (next_token_predict == self.tokenizer.eos_token_id)
+        dones = sample_state.dones | (next_token_predict == self.tokenizer.eos_token_id) | (jnp.sum(sample_state.token_buffer[:,i-5:i]==sample_state.token_buffer[:,i],axis=1)==5)
 
         sample_state.dones=dones
 

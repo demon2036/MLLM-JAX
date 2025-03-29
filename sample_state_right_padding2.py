c1789a29fb8a4ac50faa8578ae50ec1f06e8bd5c
john <1165137798@qq.com>
2025-03-29 12:07:57 +0800

    commit

--- sample_state_right_padding2.py
+++ sample_state_right_padding2.py
@@ -322,7 +322,7 @@ class Sampler:
                                                                                                 position_ids,
                                                                                                 max_length=max_length)
 
-
+        print(logits.shape,position_ids.shape)
         next_token_logits=jnp.take_along_axis(logits,position_ids[...,None]-1,axis=1)[:,-1]
         # next_token_predict = jnp.argmax(, axis=-1)[:,0]
         next_token_predict=self.sample_fn(self.key,next_token_logits)

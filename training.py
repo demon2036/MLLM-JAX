1f286e12d9f8bee45ff710da44b962c2d569d864
john <1165137798@qq.com>
2025-03-30 15:55:25 +0800

    commit

--- training.py
+++ training.py
@@ -4,6 +4,7 @@ from typing import Any
 
 import tqdm
 from jax.experimental.multihost_utils import process_allgather
+from latex2sympy2_extended import NormalizationConfig
 
 os.environ['JAX_TRACEBACK_FILTERING']='off'
 
@@ -31,10 +32,7 @@ from MLLM_JAX.utils import get_jax_mesh2, match_partition_rules, get_partition_r
 from sample_state_right_padding import get_model, Sampler
 # from sample_state_left_padding import get_model, Sampler
 import jax.numpy as jnp
-from math_verify import parse, verify, ExprExtractionConfig
-
-
-
+from math_verify import parse, verify, ExprExtractionConfig, LatexExtractionConfig
 
 
 def slice_data(x,accumulate_steps,i):
@@ -146,16 +144,67 @@ def repeat(lst:list,repeats:int):
 
 
 
-def reward_correct(item, answer):
-    pattern = r'\d+\.\d+|\d+/\d+|\d+'
-    nums = re.findall(pattern, answer) # 使用正则表达式在answer中查找所有数字
-    if len(nums) == 0: return 0.0
-    lastnum = nums[-1] # 用answer中最后一个数字和ground_truth做比较
-    ans = parse(lastnum, extraction_config=[ExprExtractionConfig()])
-    ground_truth = parse(item["A"], extraction_config=[ExprExtractionConfig()])
-    # print(item["A"],ans)
+# def reward_correct(item, answer):
+#     pattern = r'\d+\.\d+|\d+/\d+|\d+'
+#     nums = re.findall(pattern, answer) # 使用正则表达式在answer中查找所有数字
+#     if len(nums) == 0: return 0.0
+#     lastnum = nums[-1] # 用answer中最后一个数字和ground_truth做比较
+#     ans = parse(lastnum, extraction_config=[ExprExtractionConfig()])
+#     ground_truth = parse(item["A"], extraction_config=[ExprExtractionConfig()])
+#     # print(item["A"],ans)
+#
+#     return 1 if verify(ans, ground_truth) else 0.0
 
-    return 1 if verify(ans, ground_truth) else 0.0
+
+def reward_correct(item, answer, **kwargs):
+    """Reward function that checks if the completion is the same as the ground truth."""
+    sol=item["A"]
+    content=answer
+
+    gold_parsed = parse(
+        sol,
+        extraction_mode="first_match",
+        extraction_config=[LatexExtractionConfig()],
+    )
+    if len(gold_parsed) != 0:
+        # We require the answer to be provided in correct latex (no malformed operators)
+        answer_parsed = parse(
+            content,
+            extraction_config=[
+                LatexExtractionConfig(
+                    normalization_config=NormalizationConfig(
+                        nits=False,
+                        malformed_operators=False,
+                        basic_latex=True,
+                        equations=True,
+                        boxed="all",
+                        units=True,
+                    ),
+                    # Ensures that boxed is tried first
+                    boxed_match_priority=0,
+                    try_extract_without_anchor=False,
+                )
+            ],
+            extraction_mode="first_match",
+        )
+        # Reward 1 if the content is the same as the ground truth, 0 otherwise
+        try:
+            reward = float(verify(answer_parsed, gold_parsed))
+        except Exception as e:
+            print(f"verify failed: {e}, answer: {answer_parsed}, gold: {gold_parsed}")
+            reward = 0.0
+    else:
+        # If the gold solution is not parseable, we reward 1 to skip this example
+        reward = 1.0
+        print("Failed to parse gold solution: ", sol)
+
+    return reward
+
+
+
+
+
+
 def reward_format(item, answer):
     # pattern = r"^<think>(?:(?!</?think>)[\s\S]*?)</think>\s*<answer>(?:(?!</?answer>)[\s\S]*?)</answer><\|im_end\|>$"
     # pattern = r"^<think>.*?</think>.*?<answer>.*?</answer>$"

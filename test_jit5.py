57bb15555d030962456be8ff9b8445d56402e268
john <1165137798@qq.com>
2025-04-01 15:48:00 +0800

    commit

--- test_jit5.py
+++ test_jit5.py
@@ -31,9 +31,9 @@ import jax.numpy as jnp
 
 max_prompt_length=400
 num_pre_Q=16
-MAX_LENGTH_SAMPLE=1024
+MAX_LENGTH_SAMPLE=512
 MAX_LENGTH=MAX_LENGTH_SAMPLE+512 #-128
-grad_accum_steps = 4
+grad_accum_steps = 1
 
 model_path = 'Qwen/Qwen2.5-7B'
 # model_path = 'Qwen/Qwen2.5-7B-Instruct'

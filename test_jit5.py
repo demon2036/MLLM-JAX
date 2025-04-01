2729de227edeb70754e4fb516dc3569d91ebfb08
john <1165137798@qq.com>
2025-04-01 21:39:19 +0800

    commit

--- test_jit5.py
+++ test_jit5.py
@@ -214,8 +214,8 @@ def main():
 
     temp_fn = jax.jit(functools.partial(test, train_state_sharding=train_state_sharding_cpu),
                       donate_argnums=(0,),
-                      in_shardings=(train_state_sharding_cpu, None),
-                      out_shardings=(train_state_sharding, None))
+                      in_shardings=(train_state_sharding_cpu, ),
+                      out_shardings=(train_state_sharding, ))
 
     temp_fn(state)
     print('hi')

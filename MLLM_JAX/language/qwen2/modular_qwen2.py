05a2cbe8a4b3a4566e6d692b76bfc549d1b41f05
john <1165137798@qq.com>
2025-04-01 13:21:11 +0800

    commit

--- MLLM_JAX/language/qwen2/modular_qwen2.py
+++ MLLM_JAX/language/qwen2/modular_qwen2.py
@@ -1,13 +1,19 @@
+import functools
+import math
 from typing import Any
 
+import einops
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
 import numpy as np
+from jax.experimental.pallas.ops.tpu.splash_attention import splash_attention_kernel, splash_attention_mask
+from jax.experimental.shard_map import shard_map
 
 from .configuration_qwen2 import Qwen2Config
-from ..llama.llama import LlamaMLP, LlamaAttention, LlamaDecoderLayer, LlamaRMSNorm, LlamaRotaryEmbedding
-
+from ..llama.llama import LlamaMLP, LlamaAttention, LlamaDecoderLayer, LlamaRMSNorm, LlamaRotaryEmbedding, \
+    apply_rotary_pos_emb, repeat_kv
+from jax.sharding import PartitionSpec as P
 
 class Qwen2MLP(LlamaMLP):
     config: Qwen2Config
@@ -45,6 +51,129 @@ class Qwen2Attention(LlamaAttention):
         self.k_proj = nn.Dense(self.num_key_value_heads * self.head_dim, use_bias=True)
         self.v_proj = nn.Dense(self.num_key_value_heads * self.head_dim, use_bias=True)
         self.o_proj = nn.Dense(self.hidden_size, use_bias=False)
+
+    def __call__(
+            self,
+            x: jax.Array,
+            input_ids: jax.Array,
+            cache: None,
+            attn_mask: jax.Array,
+            position_embeddings: jax.Array,
+    ) -> tuple[  None, jax.Array]:
+        bsz, q_len, _ = x.shape
+
+        query_states = self.q_proj(x)
+        key_states = self.k_proj(x)
+        value_states = self.v_proj(x)
+
+        query_states = einops.rearrange(query_states, 'b n (h d)->b h n  d ', d=self.head_dim)
+        key_states = einops.rearrange(key_states, 'b n (h d)->b h n  d ', d=self.head_dim)
+        value_states = einops.rearrange(value_states, 'b n (h d)->b h n  d ', d=self.head_dim)
+
+        """
+        if self.jax_config is not None:
+            print('sharding attention !')
+            query_states=jax.lax.with_sharding_constraint(query_states, NamedSharding(self.jax_config.mesh,PartitionSpec('dp','tp', 'fsdp',None)))
+            key_states = jax.lax.with_sharding_constraint(key_states, NamedSharding(self.jax_config.mesh,
+                                                                                        PartitionSpec('dp', 'tp',
+                                                                                                      'fsdp', None)))
+            # value_states = jax.lax.with_sharding_constraint(value_states, NamedSharding(self.jax_config.mesh,
+            #                                                                             PartitionSpec('dp', 'tp',
+            #                                                                                           'fsdp', None)))
+
+
+            value_states=nn.with_logical_constraint(value_states,('batch','embed',None,None),fallback=RulesFallback.RAISE_ERROR)
+
+        """
+
+        dtype = x.dtype
+        cos, sin = position_embeddings
+        query_states=query_states.astype(jnp.float32)
+        key_states = key_states.astype(dtype)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+        query_states = query_states.astype(dtype)
+        key_states = key_states.astype(dtype)
+        # slice_indices = (0, 0, end_index % cache['v'].shape[2], 0)
+        # jax.debug.print( "end_index % cache['v'].shape[2]=   {bar}",bar=slice_indices)
+        if cache is not None:
+            end_index = cache['end_index'][0]
+            slice_indices = (0, 0, end_index , 0)
+            value_states = jax.lax.dynamic_update_slice(
+                cache['v'],
+                value_states.astype(cache['v'].dtype),
+                slice_indices,
+            )
+            key_states = jax.lax.dynamic_update_slice(
+                cache['k'], key_states.astype(cache['k'].dtype), slice_indices
+            )
+            new_cache = {
+                'v': value_states,
+                'k': key_states,
+                'end_index': cache['end_index'] + q_len,
+            }
+
+            value_states=value_states.astype(dtype)
+            key_states=key_states.astype(dtype)
+
+        else:
+            new_cache = None
+
+
+        # print(query_states.shape,value_states.shape)
+
+        value_states=repeat_kv(value_states,self.num_key_value_groups)
+        key_states=repeat_kv(key_states,self.num_key_value_groups)
+
+        if q_len%128==0 and value_states.shape[-1]%128==0 :
+            @functools.partial(
+                shard_map,
+                mesh=self.jax_config.mesh,
+                in_specs=P(['dp','fsdp'],'tp',None,None),
+                out_specs=P(['dp','fsdp'],'tp',None,None),
+                check_rep=False,
+            )
+            def wrap_splash_attention(query_states, key_states, value_states):
+                mask = splash_attention_mask.CausalMask(shape=(key_states.shape[2], key_states.shape[2]))
+                multi_head_mask = splash_attention_mask.MultiHeadMask(masks=(mask,) * value_states.shape[1])
+
+                block_sizes = splash_attention_kernel.BlockSizes(
+                    block_q=min(512, query_states.shape[2]),
+                    block_kv_compute=min(512, key_states.shape[2]),
+                    block_kv=min(512, key_states.shape[2]),
+                    block_q_dkv=min(512, query_states.shape[2]),
+                    block_kv_dkv=min(512, key_states.shape[2]),
+                    block_kv_dkv_compute=min(512, query_states.shape[2]),
+                    block_q_dq=min(512, query_states.shape[2]),
+                    block_kv_dq=min(512, query_states.shape[2]),
+                )
+
+                splash_kernel = splash_attention_kernel.make_splash_mha(
+                    mask=multi_head_mask,
+                    head_shards=1,
+                    q_seq_shards=1,
+                    block_sizes=block_sizes
+                )
+
+                attn_output = jax.vmap(splash_kernel)(query_states , key_states, value_states)
+                return attn_output
+
+            attn_output=wrap_splash_attention(query_states/ math.sqrt(self.head_dim), key_states, value_states).astype(jnp.bfloat16)
+
+
+        else:
+
+            attn_weights = (query_states @ key_states.swapaxes(2, 3)) / math.sqrt(self.head_dim)
+            if attn_mask is not None:  # no matter the length, we just slice it
+                causal_mask = attn_mask
+                attn_weights = attn_weights + causal_mask
+
+            attn_weights = nn.softmax(attn_weights.astype(jnp.float32), axis=-1, ).astype(attn_weights.dtype)
+            attn_output = attn_weights @ value_states
+
+
+        attn_output = einops.rearrange(attn_output, 'b h n d-> b n (h d)')
+        attn_output = self.o_proj(attn_output)
+        return new_cache, attn_output.astype(dtype)
 
 
 class Qwen2DecoderLayer(LlamaDecoderLayer):

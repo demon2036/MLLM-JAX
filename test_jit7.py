6878657effe5bbf75b211c3c56980abc8c041db2
john <1165137798@qq.com>
2025-04-03 18:50:22 +0800

    commit

--- test_jit7.py
+++ test_jit7.py
@@ -159,7 +159,7 @@ def batch_process(tip_texts,answers,rewards,tokenizer, reward_corrects,  max_len
     pad_labels[:,:labels.shape[1]]=labels
 
 
-    # pad_labels=np.where(true_lengths_completions[:,None]<=1024-128,pad_labels,0)
+    pad_labels=np.where(true_lengths_completions[:,None]<=1024-128,pad_labels,0)
     # for i,true_length, in enumerate(true_lengths_prompts):
     #     if reward_corrects[i]!=1:
     #         pad_labels[i,true_length+512:]=0

12607e86bf5e39b478c94bb9a0fd84f3d42d9d43
john <1165137798@qq.com>
2025-04-12 20:06:18 +0800

    commit

--- training2.py
+++ training2.py
@@ -93,7 +93,7 @@ def get_state(mesh,training_steps=100,grad_accum_steps=1,model_path='Qwen/Qwen2.
         )
         # tx = optax.adamw(learning_rate)
         # tx = optax.lion(learning_rate,weight_decay=1e-8)
-        tx = optax.lion(learning_rate, )
+        tx = optax.lion(learning_rate,weight_decay=1e-4 )
         # tx = optax.sgd(learning_rate)
         tx = optax.chain(optax.clip_by_global_norm(1.0), tx)
         if grad_accum_steps > 1:

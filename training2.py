98cf1740753e9ff35f8cda2ff36f37c7c1f937c5
john <1165137798@qq.com>
2025-04-12 17:50:47 +0800

    commit

--- training2.py
+++ training2.py
@@ -254,7 +254,7 @@ def get_advantages(rewards,groups,advantage_estimator='grpo_clip2',alpha=0.02,me
         advantages = jnp.where(advantages > 0, advantages * scale_factors, advantages)
 
         group_min = advantages.reshape(-1, groups).min(axis=1)
-        scale_factors_neg = -1.0 / (group_min + 1e-6)
+        scale_factors_neg = -0.5 / (group_min + 1e-6)
         scale_factors_neg = jnp.repeat(scale_factors_neg, groups, axis=0)
         advantages = jnp.where(advantages < 0, advantages * scale_factors_neg, advantages)
 

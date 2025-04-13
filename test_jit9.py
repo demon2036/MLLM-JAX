96c811c6681cda172c821d44dfe7b4578ea491ee
john <1165137798@qq.com>
2025-04-13 18:45:53 +0800

    commit

--- test_jit9.py
+++ test_jit9.py
@@ -237,8 +237,8 @@ def main():
         mean_correct_length=ema_decay*mean_correct_length+(1-ema_decay)*completion_ids_global_correct.sum(axis=1).max()
 
         correct_mask_local=reward_corrects==1.0
-        tip_text_local_incorrect=tip_text_global[~correct_mask_local]
-        answers_local_incorrect = answers_global[~correct_mask_local]
+        tip_text_local_incorrect=tip_text[~correct_mask_local]
+        answers_local_incorrect = answers[~correct_mask_local]
 
         for q,a in zip(tip_text_local_incorrect,answers_local_incorrect):
             replay_buffer[q].append(a)

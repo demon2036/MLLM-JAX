cffba100b1972f3192c2dcb133cbda2b4efaf118
john <1165137798@qq.com>
2025-04-13 15:04:39 +0800

    commit

--- test_jit8.py
+++ test_jit8.py
@@ -39,8 +39,8 @@ import jax.numpy as jnp
 num_pre_Q=16
 MAX_LENGTH_SAMPLE=1024
 MAX_LENGTH=MAX_LENGTH_SAMPLE+512 #-128
-grad_accum_steps = 1
-BATCH =  2
+grad_accum_steps = 8
+BATCH =  16
 
 model_path = 'Qwen/Qwen2.5-1.5B-Instruct'
 

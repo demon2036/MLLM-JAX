8aaf651ceb5166cb16aba1cdb09bb01415e88128
john <1165137798@qq.com>
2025-04-15 22:48:55 +0800

    commit

--- test_jit10.py
+++ test_jit10.py
@@ -54,8 +54,8 @@ class TrainingConfig:
     ppo_epochs: int = 2
     mesh_shape_dp: str = "-1,1,1"
     mesh_shape_fsdp: str = "1,-1,1"
-    sample_from_buffer_prob: float = 0.5
-    initial_buffer_fill_steps: int = 10
+    sample_from_buffer_prob: float = 1.0
+    initial_buffer_fill_steps: int = 1
     # Advantage calculation alpha (for grpo_clip2)
     advantage_alpha: float = 0.02 # Added alpha for grpo_clip2
     reward_funcs_weights: Dict[str, float] = field(default_factory=dict)

81d288361d36ec513100867a83b77163f2a9f31a
john <1165137798@qq.com>
2025-04-15 13:41:59 +0800

    commit

--- MLLM_JAX/train_modules/__init__.py
+++ MLLM_JAX/train_modules/__init__.py
@@ -144,7 +144,7 @@ def get_advantages(rewards,groups,alpha=0.2,avg_entropy_per_sample=None,entropy_
     advantages = (modified_rewards - mean_grouped_mod_rewards) / (std_grouped_mod_rewards + 1e-4)
 
     group_max = advantages.reshape(-1, groups).max(axis=1)
-    scale_factors = 2.0 / (group_max + 1e-6)
+    scale_factors = 1.0 / (group_max + 1e-6)
     scale_factors = jnp.repeat(scale_factors, groups, axis=0)
     advantages = jnp.where(advantages > 0, advantages * scale_factors, advantages)
 

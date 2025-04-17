74e9693c128900f1cf77081ddb03dff7b3cc04f0
john <1165137798@qq.com>
2025-04-17 23:09:56 +0800

    commit

--- test_jit9.py
+++ test_jit9.py
@@ -576,7 +576,7 @@ def main():
         logger.info(f"Step {step}: Local Rewards Mean: {total_rewards_local.mean():.4f}, Global Rewards Mean: {mean_global:.4f}, Std: {std_global:.4f}")
 
         # --- Determine Advantage Estimator based on last_entropy ---
-        if last_entropy > 0.2 and step>300 :
+        if last_entropy > 0.2  :
             advantage_estimator = 'grpo_clip2'
         else:
             advantage_estimator = 'reinforce'

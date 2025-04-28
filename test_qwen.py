eb874d0146976968bf4cde168a129d182c09279a
john <1165137798@qq.com>
2025-04-28 12:13:03 +0800

    commit

--- test_qwen.py
+++ test_qwen.py
@@ -193,7 +193,7 @@ class Sampler:
         self.jit_infer_step = jax.jit(self.infer)
         self.sample_fn=functools.partial(_temperature_sampling,t=0.6)
         self.prefill_bucket = [
-            128, 256, 512, 1024, 2048, 4096,8192,16384,int(16384*1.5),16384*2
+            128, 256, 512, 1024, 2048, 4096,8192,16384,int(16384*1.5),16384*2-1024
         ]
 
 
